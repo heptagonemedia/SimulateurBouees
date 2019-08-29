0854a5c739fe42e2d51b6f352b8a5a0528097e2b
Adrian <adrian.paul.carrieres@gmail.com>
2019-08-29 15:44:01 -0400

    Ajout restrictions sur generation de la temp

--- src/donnee/Bouee.java
+++ src/donnee/Bouee.java
@@ -19,7 +19,9 @@ public class Bouee {
 	private ValeursInitiales valeursInitiales;
 	
 	private ArrayList<PointDonnee> listePointDonnee;
-	private Random generateurDeHasard; 
+	private Random generateurDeHasard;
+
+	private float[][] tabTempMinMax;
 	
 	public Bouee(Integer numero, String description) {
 		generateurDeHasard = new Random();
@@ -34,6 +36,8 @@ public class Bouee {
 		no_seq = 0;
 		
 		listePointDonnee = new ArrayList<PointDonnee>();
+
+		tabTempMinMax = remplirTabTemp();
 	}
 	
 	public void demarrerCollecte(Integer intervale) {
@@ -61,7 +65,11 @@ public class Bouee {
 		
 		p.setId_bouee(numero);
 		p.setMoment(new Date().getTime());
-		p.setTemperature(valeursInitiales.getTemperature() - 10 + (generateurDeHasard.nextFloat()*10));
+
+		//p.setTemperature(valeursInitiales.getTemperature() - 10 + (generateurDeHasard.nextFloat()*10));
+		p.setTemperature(generateurDeHasard.nextFloat() * ((tabTempMinMax[0][1] - tabTempMinMax[0][0]) + 1) + tabTempMinMax[0][0]);
+
+
 		p.setSalinite(valeursInitiales.getSalinite() - 5 + (generateurDeHasard.nextFloat() * 5));
 		p.setDebit(valeursInitiales.getDebit() - 1 + (generateurDeHasard.nextFloat() * 2));
 		p.setLongitude(valeursInitiales.getLongitude() + ((-0.5 + generateurDeHasard.nextFloat())/10) );
@@ -78,5 +86,47 @@ public class Bouee {
 			listePointDonneeLocale.add(this.genererPointDonnee());
 		}
 		return listePointDonneeLocale;
+	}
+
+	public float[][] remplirTabTemp(){
+		float[][] tab = new float[12][2];
+
+		tab[0][0] = 0;
+		tab[0][1] = 5;
+
+		tab[1][0] = 0;
+		tab[1][1] = 3;
+
+		tab[2][0] = 3;
+		tab[2][1] = 5;
+
+		tab[3][0] = 5;
+		tab[3][1] = 7;
+
+		tab[4][0] = 7;
+		tab[4][1] = 17;
+
+		tab[5][0] = 15;
+		tab[5][1] = 20;
+
+		tab[6][0] = 20;
+		tab[6][1] = 25;
+
+		tab[7][0] = 23;
+		tab[7][1] = 25;
+
+		tab[8][0] = 17;
+		tab[8][1] = 23;
+
+		tab[9][0] = 10;
+		tab[9][1] = 20;
+
+		tab[10][0] = 7;
+		tab[10][1] = 15;
+
+		tab[11][0] = 3;
+		tab[11][1] = 10;
+
+		return tab;
 	}
 }
